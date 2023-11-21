8b89245e8006ae659486f9fe2b53b0ba63fb1b0b
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-11-21 18:06:51 -0300

    added footer component

---
+++ almoxarifado/src/components/component/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Footer() {
+  return <div className='bg-primary p-3 m-0 d-flex justify-content-center align-content-center align-items-center'></div>
+}
+
+export default Footer
