e9d4bb0bc1c4e39dfc8ac85b7383e79fb3cb9fef
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-11-22 10:21:12 -0300

    adjust theme reaction  in toggle menu

--- almoxarifado/src/components/component/Menu.jsx
+++ almoxarifado/src/components/component/Menu.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import "./styles.css";
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,9 +12,20 @@ import {
 
 const Menu = () => {
     const [show, setShow] = useState(false);
+    const [theme, setTheme] = useState("dark");
 
     const handleClose = () => setShow(false);
-    const toggleShow = () => setShow((s) => !s);
+    const toggleShow = () => {
+        setShow((s) => !s);
+        const getThemeFromLocalStorage = () => {
+            const storedTheme = localStorage.getItem('theme');
+            if (storedTheme) {
+                setTheme(storedTheme);
+            }
+        };
+
+        getThemeFromLocalStorage();
+    };
 
     const menuTooltip = (
         <Tooltip id="menu-tooltip">
@@ -22,15 +33,27 @@ const Menu = () => {
         </Tooltip>
     );
 
+    useEffect(() => {
+        const getThemeFromLocalStorage = () => {
+            const storedTheme = localStorage.getItem('theme');
+            if (storedTheme) {
+                setTheme(storedTheme);
+            }
+        };
+
+        getThemeFromLocalStorage();
+    }, []);
+
     return (
         <>
             <OverlayTrigger placement="bottom" overlay={menuTooltip}>
                 <MDBBtn
                     variant="primary"
                     onClick={toggleShow}
+
                     className='m-0 p-0 shadow d-flex justify-content-center align-content-center align-items-center fw-bold rounded-5 border'
                 >
-                    <FontAwesomeIcon icon={faBars} className='text-white shadow m-0 p-2 fw-bold fs-2' />
+                    <FontAwesomeIcon icon={faBars} className=' shadow m-0 p-2 fw-bold fs-2' />
                 </MDBBtn >
             </OverlayTrigger>
             <Offcanvas
@@ -43,13 +66,13 @@ const Menu = () => {
                 autoFocus
                 enforceFocus
                 restoreFocus
-                className="border-0 bg-primary hover-menu menu-tamanho border-black w-auto shadow-6-strong"
+                className={`m-1 hover-menu menu-tamanho w-auto shadow-6-strong ${theme === "dark" ? "bg-dark data-bs-theme-dark text-white border border-white" : "bg-light data-bs-theme-light text-black border border-dark"}`}
             >
                 <Offcanvas.Header
                     closeButton
-                    closeVariant='white'
+                    closeVariant={`${theme === "dark" ? "white" : ""}`}
                 >
-                    <Offcanvas.Title className="fs-4 fw-bold text-white text-store-nav" >
+                    <Offcanvas.Title className="fs-4 fw-bold" >
                         Menu
                     </Offcanvas.Title>
                 </Offcanvas.Header>
@@ -59,16 +82,16 @@ const Menu = () => {
                         <li className="m-3">
                             <Link to="/retirar">
                                 <MDBBtn
-                                    className='menu-btn-tamanho btn-success border rounded-9 shadow-5-strong shadow-5-primary d-flex justify-content-center align-content-center align-items-center'>
+                                    className={`${theme === "dark" ? "border border-2 border-white" : "border border-2 border-dark"} menu-btn-tamanho btn-success rounded-9 shadow-5-strong shadow-5-primary d-flex justify-content-center align-content-center align-items-center`}>
                                     <div className="d-flex flex-row align-items-center p-0 m-0 gap-3">
                                         <div className="d-flex flex-column col-3 justify-content-start align-content-start">
                                             <FontAwesomeIcon
                                                 icon={faCubes}
-                                                className="text-white me-2 fs-3 text-start"
+                                                className=" me-2 fs-3 text-start"
                                             />
                                         </div>
                                         <div className="d-flex col-9 justify-content-center align-content-center">
-                                            <h6 className="p-0 m-0 text-white text-center ">Retirada</h6>
+                                            <h6 className="p-0 m-0  text-center ">Retirada</h6>
                                         </div>
                                     </div>
                                 </MDBBtn>
@@ -77,16 +100,16 @@ const Menu = () => {
                         <li className="m-3">
                             <Link to="/entrada">
                                 <MDBBtn
-                                    className='menu-btn-tamanho bg-primary-emphasis border rounded-9 shadow-5-strong shadow-5-primary d-flex justify-content-center align-content-center align-items-center'>
+                                    className={`${theme === "dark" ? "border border-2 border-white" : "border border-2 border-dark"} menu-btn-tamanho bg-primary-emphasis rounded-9 shadow-5-strong shadow-5-primary d-flex justify-content-center align-content-center align-items-center`}>
                                     <div className="d-flex flex-row align-items-center p-0 m-0 gap-3">
                                         <div className="d-flex flex-column col-3 justify-content-start align-content-start">
                                             <FontAwesomeIcon
                                                 icon={faShareFromSquare}
-                                                className="text-white me-2 fs-3 text-start"
+                                                className=" me-2 fs-3 text-start"
                                             />
                                         </div>
                                         <div className="d-flex col-9 justify-content-center align-content-center">
-                                            <h6 className="p-0 m-0 text-white text-center ">Entrada</h6>
+                                            <h6 className="p-0 m-0  text-center ">Entrada</h6>
                                         </div>
                                     </div>
                                 </MDBBtn>
@@ -95,16 +118,16 @@ const Menu = () => {
                         <li className="m-3">
                             <Link to="/alterar">
                                 <MDBBtn
-                                    className='menu-btn-tamanho btn-warning border rounded-9 shadow-5-strong shadow-5-primary d-flex justify-content-center align-content-center align-items-center'>
+                                    className={`${theme === "dark" ? "border border-2 border-white" : "border border-2 border-dark"} menu-btn-tamanho btn-warning rounded-9 shadow-5-strong shadow-5-primary d-flex justify-content-center align-content-center align-items-center`}>
                                     <div className="d-flex flex-row align-items-center p-0 m-0 gap-3">
                                         <div className="d-flex flex-column col-3 justify-content-start align-content-start">
                                             <FontAwesomeIcon
                                                 icon={faBoxesStacked}
-                                                className="text-white me-2 fs-3 text-start"
+                                                className=" me-2 fs-3 text-start"
                                             />
                                         </div>
                                         <div className="d-flex col-9 justify-content-center align-content-center">
-                                            <h6 className="p-0 m-0 text-white text-center ">Alterar</h6>
+                                            <h6 className="p-0 m-0  text-center ">Alterar</h6>
                                         </div>
                                     </div>
                                 </MDBBtn>
@@ -113,16 +136,16 @@ const Menu = () => {
                         <li className="m-3">
                             <Link to="/desativar">
                                 <MDBBtn
-                                    className='menu-btn-tamanho btn-danger border rounded-9 shadow-5-strong shadow-5-primary d-flex justify-content-center align-content-center align-items-center'>
+                                    className={`${theme === "dark" ? "border border-2 border-white" : "border border-2 border-dark"} menu-btn-tamanho btn-danger rounded-9 shadow-5-strong shadow-5-primary d-flex justify-content-center align-content-center align-items-center`}>
                                     <div className="d-flex flex-row align-items-center p-0 m-0 gap-3">
                                         <div className="d-flex flex-column col-3 justify-content-start align-content-start">
                                             <FontAwesomeIcon
                                                 icon={faPlugCircleExclamation}
-                                                className="text-white me-2 fs-3 text-start"
+                                                className=" me-2 fs-3 text-start"
                                             />
                                         </div>
                                         <div className="d-flex col-8 justify-content-center align-content-center">
-                                            <h6 className="p-0 m-0 text-white text-center ">Desativar</h6>
+                                            <h6 className="p-0 m-0  text-center ">Desativar</h6>
                                         </div>
                                     </div>
                                 </MDBBtn>
@@ -131,16 +154,16 @@ const Menu = () => {
                         <li className="m-3">
                             <Link to="/listar">
                                 <MDBBtn
-                                    className='menu-btn-tamanho btn-info border rounded-9 shadow-5-strong shadow-5-primary d-flex justify-content-center align-content-center align-items-center'>
+                                    className={`${theme === "dark" ? "border border-2 border-white" : "border border-2 border-dark"} menu-btn-tamanho btn-info rounded-9 shadow-5-strong shadow-5-primary d-flex justify-content-center align-content-center align-items-center`}>
                                     <div className="d-flex flex-row align-items-center p-0 m-0 gap-3">
                                         <div className="d-flex flex-column col-3 justify-content-start align-content-start">
                                             <FontAwesomeIcon
                                                 icon={faTableCells}
-                                                className="text-white me-2 fs-3 text-start"
+                                                className=" me-2 fs-3 text-start"
                                             />
                                         </div>
                                         <div className="d-flex col-9 justify-content-center align-content-center">
-                                            <h6 className="p-0 m-0 text-white text-center ">Listar</h6>
+                                            <h6 className="p-0 m-0  text-center ">Listar</h6>
                                         </div>
                                     </div>
                                 </MDBBtn>
