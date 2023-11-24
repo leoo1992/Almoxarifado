3973ad34af341cc517969b2fdf9a643de006227b
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-11-24 15:27:18 -0300

    add cart body and footer

--- almoxarifado/src/components/component/Cart.jsx
+++ almoxarifado/src/components/component/Cart.jsx
@@ -2,10 +2,11 @@ import { React, useState, useEffect } from 'react';
 import './styles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
-import { MDBBtn } from 'mdb-react-ui-kit';
+import { MDBBtn, MDBRow, MDBContainer, MDBBtnGroup } from 'mdb-react-ui-kit';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import ProductCard from './ProductCard';
 
 function Cart() {
 	const totalCounters = useState(999);
@@ -13,7 +14,7 @@ function Cart() {
 	const [theme, setTheme] = useState('dark');
 	const handleCloseCart = () => setShowCart(false);
 	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-	
+
 	const toggleShowCart = () => {
 		setShowCart((s) => !s);
 		const getThemeFromLocalStorage = () => {
@@ -28,7 +29,7 @@ function Cart() {
 
 	const cartTooltip = (
 		<Tooltip id="cartTooltip">
-			Menu
+			Carrinho
 		</Tooltip>
 	);
 
@@ -60,12 +61,12 @@ function Cart() {
 					rounded
 					className="m-0 p-0 d-flex justify-content-center align-content-center align-items-center fw-bold shadow border bg-primary "
 					onClick={toggleShowCart}
-					size= {windowWidth < 370 ? 'sm' : 'lg'}
+					size={windowWidth < 370 ? 'sm' : 'lg'}
 					color="none"
 				>
 					<span className="ms-1 m-0 badge rounded-9 text-white fw-bold rounded-circle bg-success">{totalCounters}</span>
 					<FontAwesomeIcon icon={faShoppingBasket}
-						className={`text-white m-0 p-2 ${windowWidth < 370 ? 'fs-6 ' : 'fs-3'}`} 
+						className={`text-white m-0 p-2 ${windowWidth < 370 ? 'fs-6 ' : 'fs-3'}`}
 					/>
 				</MDBBtn>
 			</OverlayTrigger >
@@ -79,21 +80,45 @@ function Cart() {
 				autoFocus
 				enforceFocus
 				restoreFocus
-				className={`m-1 hover-menu menu-tamanho w-auto shadow-6-strong ${theme === 'dark' ? 'bg-dark data-bs-theme-dark text-white border border-white' : 'bg-light data-bs-theme-light text-black border border-dark'}`}
+				className={`m-0 rounded-3 hover-menu menu-tamanho w-auto shadow-6-strong ${theme === 'dark' ? 'bg-dark data-bs-theme-dark text-white border border-white' : 'bg-light data-bs-theme-light text-black border border-dark'}`}
 			>
 				<Offcanvas.Header
 					closeButton
-					closeVariant={`${theme === 'dark' ? 'white' : ''}`}
+					className='p-2 bg-primary text-white fw-bold rounded-top-3 border m-0'
+					closeVariant='white'
 				>
-					<Offcanvas.Title className="fs-4 fw-bold" >
-            Carrinho
+					<Offcanvas.Title className="fs-6 fw-bold m-0 p-0 font-monospace" >
+						Carrinho
 					</Offcanvas.Title>
 				</Offcanvas.Header>
 
-				<Offcanvas.Body>
-
+				<Offcanvas.Body className='d-flex justify-content-center'>
+					<MDBContainer fluid className='m-0 p-0'>
+						<MDBRow className="p-0 m-0 d-flex justify-content-around align-content-around align-items-center row-cols-1 product-tamanho">
+							<ProductCard />
+							<ProductCard />
+							<ProductCard />
+							<ProductCard />
+							<ProductCard />
+							<ProductCard />
+							<ProductCard />
+							<ProductCard />
+						</MDBRow>
+					</MDBContainer>
 				</Offcanvas.Body>
-			</Offcanvas>
+				<div className="bg-primary p-1 m-0 d-flex justify-content-center align-content-center align-items-center rounded-bottom-3 border">
+					<MDBBtnGroup size='sm' className='shadow-5-strong border'>
+						<MDBBtn
+							className='m-0 border shadow-5-strong'
+							color='danger'
+						> Limpar</MDBBtn>
+						<MDBBtn
+							className='m-0 border shadow-5-strong'
+							color='success'
+						>Finalizar</MDBBtn>
+					</MDBBtnGroup>
+				</div>
+			</Offcanvas >
 		</>
 	);
 }
