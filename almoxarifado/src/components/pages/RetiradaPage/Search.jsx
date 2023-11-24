58e53cae81c6571b5ae3bc01eb17a8eed260d484
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-11-24 15:56:53 -0300

    adjusted search field

--- almoxarifado/src/components/pages/RetiradaPage/Search.jsx
+++ almoxarifado/src/components/pages/RetiradaPage/Search.jsx
@@ -2,15 +2,14 @@ import React, { useState, useRef, useEffect } from 'react';
 import './retirar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { MDBRow, MDBCol, MDBInput } from 'mdb-react-ui-kit';
+import { MDBRow, MDBCol, MDBInput} from 'mdb-react-ui-kit';
 
 function Search() {
 	const [isFocused, setIsFocused] = useState(false);
 	const inputRef = useRef(null);
 	const [search, setSearch] = useState('');
-	const [theme, setTheme] = useState(localStorage.getItem('theme')); 
-
- 
+	const [theme, setTheme] = useState(localStorage.getItem('theme'));
+	
 	const handleStorageChange = () => {
 		const updatedTheme = localStorage.getItem('theme');
 		if (updatedTheme) {
@@ -57,12 +56,13 @@ function Search() {
 			<MDBRow className='d-flex justify-content-end align-content-end align-items-end m-4 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 '>
 				<MDBCol
 					onClick={handleColClick}
+					style={{ cursor: 'pointer' }}
 					className={`d-flex justify-content-center align-content-center align-items-center p-4 rounded-9 m-0 gap-1 
-     rounded shadow-5-strong ${isFocused ? 'opacity-100' : 'opacity-50'} ${theme=== 'light' ? 'bg-success' :'bg-primary'}`}
+						rounded shadow-5-strong ${isFocused ? 'opacity-100' : 'opacity-50'} ${theme === 'light' ? 'bg-success' : 'bg-primary'}`}
 				>
 					<FontAwesomeIcon icon={faSearch} className='m-0 p-0 text-white' />
 					<MDBInput
-						type="text"
+						type="search"
 						size='sm'
 						id='searchfield'
 						label='Procurar'
