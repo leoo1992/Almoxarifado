39bccd4e811ae4d5a7fff5cb73864ba8216a4fb3
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-11-28 15:13:21 -0300

    adjust product card

--- almoxarifado/src/components/component/ProductCard.jsx
+++ almoxarifado/src/components/component/ProductCard.jsx
@@ -18,7 +18,6 @@ import {
 
 const ProductCard = ({ data }) => {
 
-	//eslint-disable-next-line
 	const {title, thumbnail} = data || {};
 
 	const carrinhoTooltip = (
@@ -30,21 +29,19 @@ const ProductCard = ({ data }) => {
 	const qtdTooltip = (
 		<Tooltip className="custom-tooltip4 p-0 m-0" id="add-cart">Quantidade</Tooltip>
 	);
-
 	return (
-		<MDBRow className="row-cols m-2 d-flex justify-content-between align-content-between align-items-center img-product pb-1">
+		<MDBRow className="row-cols m-2 d-flex justify-content-between align-content-between align-items-center img-product pb-5 ">
 			<MDBCol className="p-0 m-0">
-
-				<MDBCard shadow border="success" className="rounded-9 border-3 bg-transparent shadow-5-strong p-0 m-0" >
+				<MDBCard shadow border="success" className="rounded-9 border-3 bg-white shadow-5-strong p-0 m-0" >
 					{thumbnail ? (
-						<div className="rounded-9 p-0 m-0">
+						<div className="p-4 m-0 rounded-9 bg-transparent">
 							<MDBCardImage
-								src={thumbnail}
+								src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')}
 								alt={title || 'item desconhecido'}
 								tag={title || 'item desconhecido'}
 								fluid
 								position="top"
-								className="rounded-9 p-1 m-0 bg-transparent"
+								className="rounded-9 p-5 m-0 bg-transparent"
 							/>
 						</div>
 					) : (
@@ -55,12 +52,11 @@ const ProductCard = ({ data }) => {
 							/>
 						</div>
 					)}
-
-					<MDBCardOverlay className="d-flex flex-column align-content-between justify-content-between align-items-stretch p-3">
+					<MDBCardOverlay className="d-flex flex-column align-content-between justify-content-between align-items-stretch p-2">
 						<MDBRow className="p-0 m-0 row-cols p-0 m-0 d-flex justify-content-center 
                                             align-content-center align-items-center text-center">
-							<MDBCard className="rounded-9 shadow-5-strong bg-primary bg-opacity-0 border border-3 border-success p-0">
-								<MDBCardTitle className="p-2 m-0 text-center text-white fw-bolder fs-6">
+							<MDBCard className="rounded-7 shadow-5-strong bg-dark opacity-80 border border-3 border-success p-0">
+								<MDBCardTitle className="p-2 m-0 text-center text-white fw-bolder fs-6 bg-transparent">
 									{title || 'item desconhecido'}
 								</MDBCardTitle>
 							</MDBCard >
@@ -72,17 +68,16 @@ const ProductCard = ({ data }) => {
 									type="number"
 									placeholder="Qtd"
 									className=" tamanho-inputs p-1 mb-1 rounded-9 text-center bg-body-secondary 
-                                    shadow-5-strong border border-primary border-3"
+                                    shadow-5-strong border border-primary border-3 opacity-80"
 								/>
 							</OverlayTrigger>
-
 							<OverlayTrigger placement="top" overlay={infoTooltip}>
 								<MDBBtn
 									size="sm"
 									className="border border-3 border-primary shadow-5-primary 
                                                 fw-bolder p-0 mb-1 rounded-circle w-responsive w-auto
                                                 d-flex justify-content-center align-content-center align-items-center 
-                                                text-center btn-info shadow-5-strong">
+                                                text-center btn-info shadow-5-strong opacity-80">
 									<FontAwesomeIcon icon={faCircleInfo} className="p-1 m-0 fs-4" />
 								</MDBBtn>
 							</OverlayTrigger>
@@ -92,11 +87,10 @@ const ProductCard = ({ data }) => {
 									className="border border-3 border-success shadow-5-primary 
                                                 fw-bolder p-0 mb-1 rounded-circle w-responsive w-auto
                                                 d-flex justify-content-center align-content-center align-items-center 
-                                                text-center shadow-5-strong">
+                                                text-center shadow-5-strong opacity-80">
 									<FontAwesomeIcon icon={faCartPlus} className="p-2 m-0 fs-6" />
 								</MDBBtn>
 							</OverlayTrigger>
-
 						</MDBRow>
 					</MDBCardOverlay>
 				</MDBCard>
