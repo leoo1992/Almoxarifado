5ea884289428cf29a1f29797cd355dfa24e9a839
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-11-28 15:12:13 -0300

    adjust footer

--- almoxarifado/src/components/component/Footer.jsx
+++ almoxarifado/src/components/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
 	return (
-		<footer className="bg-primary p-3 m-0 d-flex justify-content-center align-content-center align-items-center">
+		<footer className="bg-primary p-3 m-0 d-flex justify-content-center align-content-center align-items-center bottom-0 position-fixed w-100">
 		</footer>
 	);
 }
