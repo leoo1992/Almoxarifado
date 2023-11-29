7903b4f7e5d047b70c64bd9387a5d0e868f1137d
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-11-29 11:42:45 -0300

    adjust cart item

--- almoxarifado/src/components/component/CartItem.jsx
+++ almoxarifado/src/components/component/CartItem.jsx
@@ -15,24 +15,29 @@ function CartItem() {
 
 	return (
 		<MDBCard
-			shadow
-			className='mt-2 mb-3 p-0 border rounded-9 shadow-4-strong bg-body-secondary'>
-			<MDBRow className='m-0 p-0 d-flex justify-content-start align-content-start align-items-start'>
+			className='mt-0 mb-2 p-0 border rounded-9 bg-body-secondary shadow-0'>
 
-				<MDBCol className='col-10 m-0 p-0 d-flex justify-content-start align-content-center align-items-center'>
+			<MDBRow className='m-0 mt-3 p-0 d-flex justify-content-center align-content-center align-items-center'>
+
+				<MDBCol className='col-4 m-0 p-0 d-flex justify-content-start align-content-center align-items-center'>
 					<MDBCardImage src={pregoImage}
 						alt="img do produto"
-						className="img-product m-0 p-1 d-flex justify-content-center align-content-center align-items-center rounded-9 w-50"
+						className="img-product m-0 p-0 d-flex justify-content-center align-content-center align-items-center rounded-9 w-100"
 						fluid
-						shadow
 					/>
 				</MDBCol>
 
-				<MDBCol className='col-2 mt-3 p-0 d-flex justify-content-center align-content-center align-items-center '>
+				<MDBCol className='col-6 m-0 p-2'>
+					<MDBCardTitle className='m-0 p-0 fs-6 fw-normal font-monospace' >
+						asdsdassdsadaaaaaaaas
+					</MDBCardTitle>
+				</MDBCol>
+
+				<MDBCol className='col-1 m-0 p-0 d-flex justify-content-center align-content-center align-items-center '>
 					<MDBBtn
 						color='transparent'
 						size="sm"
-						className="p-0 m-0 text-danger shadow-0">
+						className="p-0 m-0 text-danger shadow-0 text-center">
 						<FontAwesomeIcon icon={faTrash} className="p-0 m-0 fs-6" />
 					</MDBBtn>
 				</MDBCol>
@@ -40,14 +45,10 @@ function CartItem() {
 			</MDBRow>
 
 			<MDBRow className='m-0 p-0 d-flex justify-content-end align-content-center align-items-center'>
-				<MDBCol className='m-0 p-0'>
-					<MDBCardTitle className='m-3 p-0 fs-6 fw-normal font-monospace' >
-						asdsdassdsadaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas
-					</MDBCardTitle>
-				</MDBCol>
+
 			</MDBRow>
 
-			<MDBRow className='mb-3 me-3 mt-1 p-0 d-flex justify-content-end align-content-center align-items-center gap-3'>
+			<MDBRow className='mb-2 me-2 mt-1 p-0 d-flex justify-content-end align-content-center align-items-center gap-3'>
 				<MDBCol className='col-1 m-0 p-0 text-center d-flex justify-content-center align-content-center align-items-center'>
 					<MDBBtn
 						color='transparent'
