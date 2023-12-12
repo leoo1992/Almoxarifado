3375283752886121f82de0689c8c8b0aac88d797
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-12-12 18:19:20 -0300

    remove unsed var

--- back-end/app.js
+++ back-end/app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const { veryJWT } = require('./middlewares/auth');
-const seedUsuarios = require ("./seeders/20231031194909-usuario");
+const seedUsuarios = require("./seeders/20231031194909-usuario");
 
 app.use(bodyParser.json());
 
@@ -17,13 +17,14 @@ const loginRouter = require("./routes/Auth/login");
 const cadastroUserRouter = require("./routes/Usuario/cadastrosUsuario");
 const deletarUserByIdRouter = require("./routes/Usuario/deletarUserById");
 
+
 app.use(express.json());
+
 app.use(cors());
 app.use(express.static('public'));
 
 sequelize.authenticate();
 sequelize.sync({ force: true }).then(async () => {
-    const seeder = require('./seeders/20231031194909-usuario');
     await seedUsuarios();
     app.listen(port);
     console.log('Servidor rodando na porta ' + port);
